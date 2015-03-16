22eb455655352ab88901e579bd90fc251dbaa244
pawel <pawel@192.168.0.10>
2015-03-16 21:17:08 +0100

    Support for unused methods

--- SuppressWarnings2/src/pl/padamski/SuppressWarnings.java
+++ SuppressWarnings2/src/pl/padamski/SuppressWarnings.java
@@ -6,22 +6,45 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SuppressWarnings {
 
-	public static void main(String[] args) throws IOException {
+	private static final String SUPPRESS_UNUSED_METHOD = "\t@SuppressWarnings(\"unused\")";
 
+	public static void main(String[] args) throws IOException {
 		new SuppressWarnings().foo(Paths.get("C:\\blad.txt"), Charset.defaultCharset());
 	}
 
 	private void foo(Path path, Charset charset) throws IOException {
 		Stream<String> lines = Files.lines(path, charset);
-
-		List<Warning> liczby = lines.map(n -> new Warning(n)).collect(Collectors.toList());
-		System.out.println(liczby.toString());
+		Map<Path, List<Warning>> warnings = lines.skip(1).map(n -> new Warning(n)).filter(n -> n.type.equals(Warning.Type.UNUSED_METHOD))
+				.collect(Collectors.groupingBy(n -> n.path));
 		lines.close();
+
+		warnings.forEach((x, y) -> addAnnotations(x, y, charset));
+	}
+
+	private Object addAnnotations(Path path, List<Warning> warnings, Charset charset) {
+		try {
+			List<String> lines = Files.readAllLines(path, charset);
+
+			for (int i = 0; i < warnings.size(); i++) {
+				lines.add(warnings.get(i).lineNo - 1, SUPPRESS_UNUSED_METHOD);
+				for (int j = i + 1; j < warnings.size(); j++) {
+					warnings.get(j).lineNo++;
+				}
+			}
+
+			Files.write(path, lines, charset);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return null;
 	}
 
 }
--- SuppressWarnings2/src/pl/padamski/Warning.java
+++ SuppressWarnings2/src/pl/padamski/Warning.java
@@ -1,25 +1,36 @@
 package pl.padamski;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Warning {
 
 	enum Type {
-		UNUSED_METHOD("The method.*from the type .* is never used locally"), OTHER, HEADER("Description	Resource.*");
+		UNUSED_METHOD("The method.*from the type .* is never used locally"), OTHER(".*");
 
-		String prefix;
+		String regex;
 
-		Type(String prefix) {
-			this.prefix = prefix;
+		Type(String regex) {
+			this.regex = regex;
 		}
 	}
 
 	Type type;
-	Path f;
-	int line;
+	Path path;
+	int lineNo;
 
 	public Warning(String line) {
-		// TODO Auto-generated constructor stub
-	}
+		String[] columns = line.split("\t");
 
+		for (Type t : Type.values()) {
+			if (columns[0].matches(t.regex)) {
+				type = t;
+				break;
+			}
+		}
+
+		path = Paths.get(columns[2].substring(1), columns[1]);
+		lineNo = Integer.parseInt(columns[3].substring(5));
+
+	}
 }
